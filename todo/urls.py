e3ab4040901604efd88de57d77f6620a6b83889f
sochun98 <sochun98@naver.com>
2024-10-28 13:12:13 +0900

    20241028 Ch10
    Generics basic 2

--- todo/urls.py
+++ todo/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
-from todo.apis import TodoCreateAPI, TodoDeleteAPI, TodoGenericsCreateAPI, TodoGenericsListAPI, TodoGenericsListCreateAPI, TodoListAPI, TodoRetrieveAPI, TodoUpdateAPI
+from todo.apis import TodoCreateAPI, TodoDeleteAPI, TodoGenericsCreateAPI, TodoGenericsDeleteAPI, TodoGenericsListAPI, TodoGenericsListCreateAPI, TodoGenericsRetrieveAPI, TodoGenericsRetrieveUpdateDeleteAPI, TodoGenericsUpdateAPI, TodoListAPI, TodoRetrieveAPI, TodoUpdateAPI
 from todo.views import todo_list, todo_detail, todo_detail_name
 
 # 127.0.0.1:8000/todo/
 urlpatterns = [
     path("generics/", TodoGenericsListCreateAPI.as_view()),
+    path("generics/<int:pk>/", TodoGenericsRetrieveUpdateDeleteAPI.as_view()),
     path("generics/create/", TodoGenericsCreateAPI.as_view()),
     path("generics/list/", TodoGenericsListAPI.as_view()),
+    path("generics/retrieve/<int:pk>/", TodoGenericsRetrieveAPI.as_view()),
+    path("generics/update/<int:pk>/", TodoGenericsUpdateAPI.as_view()),
+    path("generics/delete/<int:pk>/", TodoGenericsDeleteAPI.as_view()),
     path("create/", TodoCreateAPI.as_view()),
     path("list/", TodoListAPI.as_view()),
     path("retrieve/<int:pk>/", TodoRetrieveAPI.as_view()),
